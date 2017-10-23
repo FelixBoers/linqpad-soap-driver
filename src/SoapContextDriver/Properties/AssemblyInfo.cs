b63da3d4e8849bc0aef7505f3c0d867fd34a41e5
Felix Boers <felix.boers@cbc-extern.de>
2017-10-23 17:49:15 +0200

    Version to 1.3.0

--- src/SoapContextDriver/Properties/AssemblyInfo.cs
+++ src/SoapContextDriver/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SoapContextDriver")]
@@ -12,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("01f8aabf-86e2-4f7d-b48f-8e030230e240")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
